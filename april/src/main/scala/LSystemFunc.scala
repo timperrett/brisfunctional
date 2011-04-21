0b35193d5ffbca0c7c6a2c84c2374ffa2eddec4e
timperrett <hello@timperrett.com>
2011-04-21 02:19:42 +0100

    All tests now pass

--- april/src/main/scala/LSystemFunc.scala
+++ april/src/main/scala/LSystemFunc.scala
@@ -2,29 +2,15 @@ package april
 
 object LSystemFuncs {
   
-  def replace(what: String)(rules: List[(Char,String)]): String = 
+  def replace(rules: List[(Char,String)])(what: String): String = 
     rules.foldLeft(what)((seed,tuple) => 
-      seed.replaceAll(what,tuple._2))
+      seed.replaceAll(tuple._1.toString,tuple._2))
 
-  def recurse(times: Int)(what: => String): String = {
-    def rpt(times: Int, what: => String, aggregator: String): String = {
-      println("Agg: " + aggregator)
-      times match {
-        case 1 => println("Once"); what
-        case _ => println("Recursing"); rpt(times - 1, what, aggregator + what)
-      }
+  def recurse(times: Int, what: String)(how: String => String): String = {
+    def rpt(times: Int, previous: => String): String = times match {
+      case 0 => previous
+      case _ => rpt(times - 1, how(previous))
     }
-    rpt(times, what, "")
+    rpt(times, what)
   }
-  
-  // def repeat(times: Int = 0)(func: => String): String = times match {
-  //     case _ if times == 1 => func
-  //     case _ => repeat(times - 1)(func) + func
-  //   }
-  
-  
-    // if(times == 1) func
-    // else repeat()
-  // (for(i <- 0 until times) 
-      // yield func).mkString
 }
--- april/src/test/scala/LSystemFuncSpec.scala
+++ april/src/test/scala/LSystemFuncSpec.scala
@@ -7,42 +7,47 @@ class LSystemSpecs extends Specification {
   
   "An empty rule map" should  {
     "not change the input A" in {
-      replace("A")(Nil) must be equalTo "A"
+      replace(Nil)("A") must be equalTo "A"
     }
   }
   "A rule map of A -> AA" should {
     "duplicate an A" in {
-      replace("A")(List('A' -> "AA")) must be equalTo "AA"
+      replace(List('A' -> "AA"))("A") must be equalTo "AA"
     }
   }
   "A rule map of A -> B" should {
     "yield B for input A " in {
-      replace("A")(List('A' -> "B")) must be equalTo "B"
+      replace(List('A' -> "B"))("A") must be equalTo "B"
     }
   }
   "A rule map of A->ABCD" should {
     "yield ABCD for input A " in {
-      replace("A")(List('A' -> "ABCD")) must be equalTo "ABCD"
+      replace(List('A' -> "ABCD"))("A") must be equalTo "ABCD"
+    }
+  }
+  "A rule map of A->ABCD,Z->B" should {
+    "yield ABCD for input A " in {
+      replace(List('A' -> "ABCD", 'Z' -> "B"))("A") must be equalTo "ABCD"
     }
   }
   "A rule map of A->ABCD itterated twice" should {
     "yield ABCDBCD for input A" in {
-      recurse(2){
-        replace("A")(List('A' -> "ABCD"))
+      recurse(2, "A"){
+        replace(List('A' -> "ABCD"))
       } must be equalTo "ABCDBCD"
     }
   }
   "A rule map of A->ABCD,A->B" should {
     "yield BBCD for input A " in {
-      recurse(1){
-        replace("A")(List('A' -> "ABCD", 'A' -> "B"))
+      recurse(1, "A"){
+        replace(List('A' -> "ABCD", 'A' -> "B"))
       } must be equalTo "BBCD"
     }
   }
   "A rule map of A->ABCD,A->B, itterated twice" should {
     "yield BBCD for two iterations" in {
-      recurse(2){
-        replace("A")(List('A' -> "ABCD", 'A' -> "B"))
+      recurse(2, "A"){
+        replace(List('A' -> "ABCD", 'A' -> "B"))
       } must be equalTo "BBCD"
     }
   }
